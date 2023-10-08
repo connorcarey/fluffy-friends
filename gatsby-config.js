63bc36134e03526031f7102246235e5cf4a52fcd
corner <92468659+cornball24@users.noreply.github.com>
2023-10-08 02:21:41 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Fluffy Friends`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
